c306f09f4ece4997143287c798a475e61b345bcf
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-05-18 07:20:03 +0100

    feat: log when prepare step is run

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import * as pinst from "pinst";
 
 export async function prepare(
     {pkgRoot}: {readonly pkgRoot?: string},
-    {cwd}: {readonly cwd: string}
+    {
+        cwd,
+        logger
+    }: {readonly cwd: string; readonly logger: {readonly log: (...args: string[]) => void}}
 ): Promise<void> {
+    logger.log("Disabling postinstall script");
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
     pinst.disableAndSave(pkgRoot == null ? cwd : resolve(cwd, String(pkgRoot)));
 }
