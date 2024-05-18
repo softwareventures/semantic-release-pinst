09d5423b0caeece04c4f49ff166584d4cd7b8ba8
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-05-18 08:38:36 +0100

    feat: re-enable postinstall script after semantic-release is complete

--- index.ts
+++ index.ts
@@ -2,17 +2,41 @@ import * as path from "node:path";
 import {fork} from "node:child_process";
 
 export async function prepare(
-    {pkgRoot}: {readonly pkgRoot?: string},
+    {pkgRoot}: {readonly pkgRoot?: string | undefined | null},
     {
         cwd,
         logger
     }: {readonly cwd: string; readonly logger: {readonly log: (...args: string[]) => void}}
 ): Promise<void> {
+    logger.log("Disabling postinstall script");
+    return pinst(pkgRoot == null ? cwd : path.resolve(cwd, String(pkgRoot)), "--disable");
+}
+
+export async function success(
+    {pkgRoot}: {readonly pkgRoot?: string | undefined | null},
+    {
+        cwd,
+        logger
+    }: {readonly cwd: string; readonly logger: {readonly log: (...args: string[]) => void}}
+): Promise<void> {
+    logger.log("Re-enabling postinstall script");
+    return pinst(pkgRoot == null ? cwd : path.resolve(cwd, String(pkgRoot)), "--enable");
+}
+
+export async function fail(
+    {pkgRoot}: {readonly pkgRoot?: string | undefined | null},
+    {
+        cwd,
+        logger
+    }: {readonly cwd: string; readonly logger: {readonly log: (...args: string[]) => void}}
+): Promise<void> {
+    logger.log("Re-enabling postinstall script");
+    return pinst(pkgRoot == null ? cwd : path.resolve(cwd, String(pkgRoot)), "--enable");
+}
+
+async function pinst(cwd: string | URL | undefined, ...args: string[]): Promise<void> {
     return new Promise((resolve, reject) => {
-        logger.log("Disabling postinstall script");
-        fork(require.resolve("pinst/bin"), ["--disable"], {
-            cwd: pkgRoot == null ? cwd : path.resolve(cwd, String(pkgRoot))
-        })
+        fork(require.resolve("pinst/bin"), args, {cwd})
             .once("error", reject)
             .once("exit", (code, signal) => {
                 if (signal != null) {
