df8d72b1dce783a75c92b9f04eb8544843f7e514
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-03-06 16:24:31 +0000

    refactor: move all functions to index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import {resolve} from "node:path";
-import {disableAndSave, enableAndSave} from "./pinst";
+import path, {resolve} from "node:path";
+import {readFile, writeFile} from "node:fs/promises";
+import {hasProperty} from "unknown";
 
 export async function prepare(
     {pkgRoot}: {readonly pkgRoot?: string | undefined | null},
@@ -28,3 +29,50 @@ function resolvePackageDir(pkgRoot: string | undefined | null, cwd: string): str
 }
 
 export const fail = success;
+
+async function rewritePackageJson(
+    dir: string,
+    fn: (packageJson: unknown) => unknown
+): Promise<void> {
+    const file = path.join(dir, "package.json");
+    const text = await readFile(file, "utf-8");
+    const indent = /^ +|\t+/mu.exec(text)?.[0];
+
+    const updated = fn(JSON.parse(text) as unknown);
+    const updatedText = JSON.stringify(updated, null, indent);
+    return writeFile(file, `${updatedText}\n`);
+}
+
+function renameScripts(packageJson: unknown, rename: (name: string) => string): unknown {
+    if (
+        typeof packageJson === "object" &&
+        hasProperty(packageJson, "scripts") &&
+        typeof packageJson.scripts === "object" &&
+        packageJson.scripts != null
+    ) {
+        return {
+            ...packageJson,
+            scripts: Object.fromEntries(
+                Object.entries(packageJson.scripts).map(
+                    ([key, value]) => [rename(key), value] as const
+                )
+            )
+        };
+    } else {
+        return packageJson;
+    }
+}
+
+async function enableAndSave(dir = process.cwd()): Promise<void> {
+    return rewritePackageJson(dir, pkg =>
+        renameScripts(pkg, name =>
+            ["_install", "_postinstall"].includes(name) ? name.substring(1) : name
+        )
+    );
+}
+
+async function disableAndSave(dir = process.cwd()): Promise<void> {
+    return rewritePackageJson(dir, pkg =>
+        renameScripts(pkg, name => (["install", "postinstall"].includes(name) ? `_${name}` : name))
+    );
+}
